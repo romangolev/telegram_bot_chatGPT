3758804ee4df175d74861194bce3bae13d6a7f43
rv.golev <rv.golev@gmail.com>
2023-04-25 00:47:32 +0300

    added command that show cpu temparature

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import openai
 import telebot
 import os
 from dotenv import load_dotenv
+from gpiozero import CPUTemperature
+
+# Commands Lists:
+#test - Test 
+#start - Start GPT Chat
+#cpu_temp - Get CPU Temp
 
 load_dotenv()
+cpu = CPUTemperature()
 
 # Set OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,9 +23,9 @@ bot = telebot.TeleBot(os.getenv("TELEGRAMBOT_API_KEY"))
 def send_start(message):
     bot.reply_to(message, "Welcome to ChatGPT! Type anything to get started.")
 
-@bot.message_handler(commands=['hi'])
-def send_hi(message):
-    bot.reply_to(message, "Hi there.")
+@bot.message_handler(commands=['cpu_temp'])
+def send_cpu_temp(message):
+    bot.reply_to(message, cpu.temperature)
 
 @bot.message_handler(commands=['test'])
 def send_welcome(message):
